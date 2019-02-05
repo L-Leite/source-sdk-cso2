742c4e756a470250a82048698339269800a808df
Ochii <ochii@leite.xyz>
2019-02-05 22:26:11 +0000

    Remove appframework, tier1, 2, 3 libraries and utils

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # source-sdk-cso2
 
-This is a partial SDK for Nexon's Counter-Strike: Online 2.
+This is an incomplete SDK for Nexon's Counter-Strike: Online 2.
 
 Read LICENSE for legal information.
--- public/tier1/characterset.h
+++ public/tier1/characterset.h
@@ -28,7 +28,27 @@ struct characterset_t
 // Input  : *pSetBuffer - pointer to the buffer for the group
 //			*pSetString - list of characters to flag
 //-----------------------------------------------------------------------------
-extern void CharacterSetBuild( characterset_t *pSetBuffer, const char *pSetString );
+//-----------------------------------------------------------------------------
+// Purpose: builds a simple lookup table of a group of important characters
+// Input  : *pParseGroup - pointer to the buffer for the group
+//			*pGroupString - null terminated list of characters to flag
+//-----------------------------------------------------------------------------
+inline void CharacterSetBuild( characterset_t* pSetBuffer, const char* pszSetString )
+{
+    int i = 0;
+
+    // Test our pointers
+    if ( !pSetBuffer || !pszSetString )
+        return;
+
+    memset( pSetBuffer->set, 0, sizeof( pSetBuffer->set ) );
+
+    while ( pszSetString[i] )
+    {
+        pSetBuffer->set[(unsigned)pszSetString[i]] = 1;
+        i++;
+    }
+}
 
 
 //-----------------------------------------------------------------------------
